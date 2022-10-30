a8c74678c8967193beb3a969f92487536ed612fc
Andrew Qiao <andrewqiao2004@gmail.com>
2022-10-30 12:33:13 -0400

    Start landing and add dummy routes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { Button } from "@mui/material";
+
 import starIcon from "../public/img/star.svg";
 
 export default function Home() {
   return (
     <>
       <header className={styles.header}>
-        <div>
-          <Image />
+        <div className={styles.logo}>
+          <Image src={starIcon} alt="Star icon" />
+        </div>
+        <div className={styles.buttons}>
+          <Button variant="contained">Log In</Button>
+          <Button variant="outlined">Sign Up</Button>
         </div>
       </header>
     </>
