46ae142c4dff90889f92702daa651151e28b3f71
Andrew Qiao <andrewqiao2004@gmail.com>
2022-10-30 12:27:50 -0400

    Add Roboto font

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import starIcon from "../public/img/star.svg";
+
 export default function Home() {
   return (
     <>
