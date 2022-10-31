42747a117650339c709070e0d27a4f78dcf42dbe
alexkmnsky <29873742+alexkmnsky@users.noreply.github.com>
2022-10-31 18:09:43 -0400

    Adjust spacing and add dummy course data

--- pages/home.jsx
+++ pages/home.jsx
@@ -2,25 +2,69 @@ import CourseCard from "../components/CourseCard";
 import AddIcon from '@mui/icons-material/Add';
 import { Grid, Typography, Fab, Container } from "@mui/material";
 
+const courses = [
+  {
+    "name": "CS 135",
+    "time": "10:00 AM",
+    "description": "Section 012",
+    "thumbnail": "/img/banners/cs.jpg",
+    "href": "#"
+  },
+  {
+    "name": "MATH 135",
+    "time": "10:00 AM",
+    "description": "Section 012",
+    "thumbnail": "/img/banners/proofs.jpg",
+    "href": "#"
+  },
+  {
+    "name": "MATH 137",
+    "time": "10:00 AM",
+    "description": "Section 012",
+    "thumbnail": "/img/banners/calc.jpg",
+    "href": "#"
+  },
+  {
+    "name": "PHYS 121",
+    "time": "10:00 AM",
+    "description": "Section 012",
+    "thumbnail": "/img/banners/physics.jpg",
+    "href": "#"
+  },
+  {
+    "name": "ECON 101",
+    "time": "10:00 AM",
+    "description": "Section 012",
+    "thumbnail": "/img/banners/stocks.jpg",
+    "href": "#"
+  }
+]
+
 export default function Home() {
-  return <>
-    <Grid container>
-      <Grid xs>
+  return <Container>
+    <Grid container style={{"paddingTop": "1rem", "paddingBottom": "1rem"}}>
+      <Grid item xs>
         <Typography variant="h3" component="h1">My Courses</Typography>
       </Grid>
-      <Grid xs="auto">
+      <Grid item xs="auto">
         <Fab variant="extended" color="primary">
           <AddIcon sx={{ mr: 1 }} />
           Add a Course
         </Fab>
       </Grid>
     </Grid>
-    <CourseCard
-      name="CS 135"
-      time="10:00 AM"
-      description="Section 012"
-      thumbnail="/img/banners/cs.jpg"
-      href="https://google.com"
-    />
-  </>;
+    <Grid container spacing={2}>
+      {courses.map(course => (
+        <Grid item xs={12} sm={6} md={3}>
+          <CourseCard
+            name={course.name}
+            time={course.time}
+            description={course.description}
+            thumbnail={course.thumbnail}
+            href={course.href}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  </Container>;
 }
