41cf63cde2140b70509a8708aa0e52282cad805f
alexkmnsky <29873742+alexkmnsky@users.noreply.github.com>
2022-10-31 19:16:37 -0400

    Add Join Class Dialog

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,6 +1,10 @@
+import * as React from 'react';
 import CourseCard from "../components/CourseCard";
+import JoinClassDialog from "../components/JoinClassDialog";
 import AddIcon from '@mui/icons-material/Add';
 import { Grid, Typography, Fab, Container } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const courses = [
   {
@@ -41,30 +45,56 @@ const courses = [
 ]
 
 export default function Home() {
-  return <Container>
-    <Grid container style={{"paddingTop": "1rem", "paddingBottom": "1rem"}}>
-      <Grid item xs>
-        <Typography variant="h3" component="h1">My Courses</Typography>
-      </Grid>
-      <Grid item xs="auto">
-        <Fab variant="extended" color="primary">
-          <AddIcon sx={{ mr: 1 }} />
-          Add a Course
-        </Fab>
-      </Grid>
-    </Grid>
-    <Grid container spacing={2}>
-      {courses.map(course => (
-        <Grid item xs={12} sm={6} md={3}>
-          <CourseCard
-            name={course.name}
-            time={course.time}
-            description={course.description}
-            thumbnail={course.thumbnail}
-            href={course.href}
-          />
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const [addCourseDialogOpen, setAddCourseDialogOpen] = React.useState(false);
+
+  const handleClickAddCourse = () => {
+    setAddCourseDialogOpen(true);
+  };
+
+  return <>
+    <Container>
+      <Grid container style={{ "paddingTop": "1rem" }}>
+        <Grid item xs>
+          <Typography variant="h3" component="h1">My Courses</Typography>
         </Grid>
-      ))}
-    </Grid>
-  </Container>;
+
+        {isMobile && <Grid item xs="auto">
+          <Fab variant="extended" color="primary" aria-label="add" onClick={handleClickAddCourse}>
+            <AddIcon sx={{ mr: 1 }} />
+            Add a Course
+          </Fab>
+        </Grid>}
+      </Grid>
+      <Grid container spacing={2} style={{ "paddingTop": "1rem", "paddingBottom": "2rem" }}>
+        {courses.map(course => (
+          <Grid item xs={12} sm={4} md={3}>
+            <CourseCard
+              name={course.name}
+              time={course.time}
+              description={course.description}
+              thumbnail={course.thumbnail}
+              href={course.href}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+    {!isMobile &&
+      <Fab
+        color="primary"
+        sx={{
+          position: "fixed",
+          bottom: (theme) => theme.spacing(2),
+          right: (theme) => theme.spacing(2)
+        }}
+        aria-label="add"
+        onClick={handleClickAddCourse}
+      >
+        <AddIcon />
+      </Fab>}
+      <JoinClassDialog open={addCourseDialogOpen} setOpen={setAddCourseDialogOpen}/>
+  </>;
 }
