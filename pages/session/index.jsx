54abb319ce30a934e7926d89e8800fb9455198c2
Maxwell-Hunt <max101hunt@gmail.com>
2022-11-03 11:45:40 -0400

    Added session title page front end

--- pages/session/index.jsx
+++ pages/session/index.jsx
@@ -1,3 +1,14 @@
+import Typography from '@mui/material/Typography';
+import { Button } from "@mui/material";
+import styles from "../../styles/session.module.css";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 export default function Session() {
-  return <></>;
+  return <>
+    <Button variant="contained" startIcon={<ArrowBackIcon />} className={styles.back__button}>Go Back</Button>
+    <div className={styles.main__container}>
+      <Typography variant="h1" align="center">Sept 27th - Lists</Typography>
+      <Button className={styles.start__button} variant="contained">Start Quiz</Button>
+    </div>
+  </>;
 }
