104d2bcf70b29f9a962aad9a6c4d91a69bf8ac5b
Alexander <29873742+alexkmnsky@users.noreply.github.com>
2022-11-05 02:00:58 -0400

    AttendanceCalendar MVP
    Add AttendanceCalendar support for dummy data and CustomPickersDay for custom styling

--- components/AttendanceCalendar.jsx
+++ components/AttendanceCalendar.jsx
@@ -1,23 +1,63 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import * as React from "react";
 
+import { styled } from "@mui/material/styles";
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import { PickersDay } from "@mui/x-date-pickers";
 
-export default function AttendanceCalendar() {
-  const [value, setValue] = React.useState(dayjs(new Date())); // Set current value to today
+const CustomPickersDay = styled(PickersDay, {
+  shouldForwardProp: (prop) => prop !== "selected"
+})(({ theme, selected }) => ({
+  ...(selected && {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    "&:hover, &:focus": {
+      backgroundColor: theme.palette.primary.dark
+    },
+    borderTopLeftRadius: "50%",
+    borderBottomLeftRadius: "50%",
+    borderTopRightRadius: "50%",
+    borderBottomRightRadius: "50%"
+  })
+}));
+
+export default function AttendanceCalendar({ attendanceData }) {
+
+  const [data, setData] = React.useState(attendanceData);
+
+  const isInAttendance = (date) => {
+    for (const [key, value] of Object.entries(data))
+    {
+      console.log(key);
+      if (date.isSame(dayjs(key)))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const renderPickerDay = (date, selectedDates, pickersDayProps) => {
+    const selected = isInAttendance(date);
+    return (
+      <CustomPickersDay
+        {...pickersDayProps}
+        selected={selected}
+      />
+    );
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <StaticDatePicker
         displayStaticWrapperAs="desktop"
-        label="Week picker"
         openTo="day"
-        value={value}
+        value={data}
         readOnly
-        renderInput={(params) => <TextField {...params} />}
+        disableFuture
+        renderDay={renderPickerDay}
       />
     </LocalizationProvider>
   );
--- pages/class/index.jsx
+++ pages/class/index.jsx
@@ -1,7 +1,17 @@
 import AttendanceCalendar from "../../components/AttendanceCalendar"
+import dayjs from "dayjs";
+
+const attendanceData = {
+  "2022-10-4": true,
+  "2022-10-6": false,
+  "2022-10-11": true,
+  "2022-10-13": true,
+  "2022-10-18": true,
+  "2022-10-20": true
+}
 
 export default function Class() {
   return <>
-    <AttendanceCalendar/>
+    <AttendanceCalendar attendanceData={attendanceData}/>
   </>;
 }
