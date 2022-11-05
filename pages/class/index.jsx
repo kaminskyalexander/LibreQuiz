364d24fcb9fa342f46eb205e6c2d5b2bf4f54aae
alexkmnsky <29873742+alexkmnsky@users.noreply.github.com>
2022-11-05 13:04:30 -0400

    Modify dummy data to better fit expected format

--- pages/class/index.jsx
+++ pages/class/index.jsx
@@ -2,12 +2,16 @@ import AttendanceCalendar from "../../components/AttendanceCalendar"
 import dayjs from "dayjs";
 
 const attendanceData = {
-  "2022-10-4": true,
-  "2022-10-6": false,
-  "2022-10-11": true,
-  "2022-10-13": true,
-  "2022-10-18": true,
-  "2022-10-20": true
+  "2022-10-04": {attended: true},
+  "2022-10-06": {attended: false},
+  "2022-10-11": {attended: true},
+  "2022-10-13": {attended: true},
+  "2022-10-18": {attended: true},
+  "2022-10-20": {attended: true},
+  "2022-10-25": {attended: false},
+  "2022-10-27": {attended: true},
+  "2022-11-01": {attended: true},
+  "2022-11-03": {attended: false}
 }
 
 export default function Class() {
