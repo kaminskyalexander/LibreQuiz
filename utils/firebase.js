1d0b15faf4b247d38567604b585cee330c78b8dd
Andrew Qiao <andrewqiao2004@gmail.com>
2022-11-09 20:03:51 -0500

    Install Firebase

---
+++ utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDL5SHnDZBynPMD9KxuD4uhpkzkR68Q0YQ',
+  authDomain: 'librequiz-f2888.firebaseapp.com',
+  projectId: 'librequiz-f2888',
+  storageBucket: 'librequiz-f2888.appspot.com',
+  messagingSenderId: '629515278265',
+  appId: '1:629515278265:web:af82302ba5fb9b11c7c868',
+  measurementId: 'G-9SNGZPE3NH',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
