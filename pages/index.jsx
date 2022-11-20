1a3bec2a9830d967fc24ed85242092d8c1d17193
Andrew Qiao <andrewqiao2004@gmail.com>
2022-11-20 12:49:28 -0500

    Add Google login

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,34 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-
 import Button from '@mui/material/Button';
-
 import starIcon from '../public/img/star.svg';
 import mockup from '../public/img/mockup.png';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth, provider } from '../utils/firebase';
 
 export default function Home() {
+  function signIn() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+
   return (
     <>
       <header className={styles.header}>
@@ -14,8 +36,9 @@ export default function Home() {
           <Image src={starIcon} alt="Star icon" />
         </div>
         <div className={styles.buttons}>
-          <Button variant="contained">Log In</Button>
-          <Button variant="outlined">Sign Up</Button>
+          <Button variant="contained" onClick={signIn}>
+            Log In
+          </Button>
         </div>
       </header>
       <main className={`${styles.main} container`}>
