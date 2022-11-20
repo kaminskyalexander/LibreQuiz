4960f6b593fca85a1ec9c66011669a0a68f1f17d
Alexander <29873742+alexkmnsky@users.noreply.github.com>
2022-11-20 14:35:28 -0500

    Move auth provider from index to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import { useRouter } from "next/router";
 
 import AppBar from "../components/AppBar";
+import { AuthProvider } from "../contexts/AuthContext"
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
+
+
   return (
-    <>
+    <AuthProvider>
       {router.pathname !== "/" && <AppBar />}
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 
