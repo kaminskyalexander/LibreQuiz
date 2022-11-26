58ec1c01f76da0307a0b65f19e216ee91b62d44b
alexkmnsky <29873742+alexkmnsky@users.noreply.github.com>
2022-11-25 23:15:14 -0500

    Update homepage logo and button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,23 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Button from '@mui/material/Button';
-import logo from '../public/img/star.svg';
+import Stack from '@mui/material/Stack'
+import QuizIcon from '@mui/icons-material/Quiz';
 import mockup from '../public/img/mockup.png';
 import { useAuth } from '../contexts/AuthContext';
+import { Typography } from '@mui/material';
 
 export default function Home() {
   const { signIn } = useAuth();
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.logo}>
-          <Image src={logo} alt="Star icon" />
-        </div>
+        <Stack direction="row" spacing={2}>
+          <QuizIcon sx={{fontSize: 42}}/>
+          <Typography variant="h4" component="h1">
+            LibreQuiz
+          </Typography>
+        </Stack>
         <div className={styles.buttons}>
           <Button variant="contained" onClick={signIn}>
             Log In
@@ -28,7 +33,7 @@ export default function Home() {
             Set up your class in minutes and start creating quizzes to boost
             student participation using LibreQuiz.
           </p>
-          <Button variant="contained">Get Started</Button>
+          <Button variant="contained" onClick={signIn}>Get Started</Button>
         </div>
         <Image src={mockup} className={styles.mockup} />
       </main>
